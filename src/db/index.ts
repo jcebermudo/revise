97c4ef5d551ab6c08dc4981ac218814ffb75ead9
James <jamestech718@gmail.com>
2024-12-31 17:01:26 +0800

    supabase

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,6 +1,5 @@
-import { drizzle } from "drizzle-orm/neon-http";
-import { neon } from "@neondatabase/serverless";
-import { config } from "dotenv";
-config({ path: ".env" }); // or .env.local
-const sql = neon(process.env.DATABASE_URL!);
-export const db = drizzle({ client: sql });
+import { drizzle } from 'drizzle-orm/postgres-js'
+import postgres from 'postgres'
+// Disable prefetch as it is not supported for "Transaction" pool mode 
+const client = postgres(process.env.DATABASE_URL!, { prepare: false })
+export const db = drizzle({ client });
